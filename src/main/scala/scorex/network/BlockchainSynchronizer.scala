7359318aed457a14e191a2de263a1b994249c71a
Sergey Nazarov <phearnot@renee.ru>
2017-05-18 21:17:44 +0300

    Resolved conflicts

--- src/main/scala/scorex/network/BlockchainSynchronizer.scala
+++ src/main/scala/scorex/network/BlockchainSynchronizer.scala
@@ -104,7 +104,6 @@ class BlockchainSynchronizer(network: Network, coordinator: ActorRef, history: H
       val overlap = withTail.lastTwoBlockIds
 
       run(initial, GettingExtensionTail) { updatedPeersData =>
-        updatedPeersData.active
         GetSignatures(overlap.reverse.map(_.blockId))
 //        networkControllerRef ! NetworkController.SendToNetwork(msg, SendToChosen(updatedPeersData.active))
 
