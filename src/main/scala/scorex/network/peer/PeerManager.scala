3f164e35fb365241189f1090d31bcf5112e5ec1e
Sergey Nazarov <phearnot@renee.ru>
2017-05-19 13:08:50 +0300

    Removed unused code

--- src/main/scala/scorex/network/peer/PeerManager.scala
+++ src/main/scala/scorex/network/peer/PeerManager.scala
@@ -2,40 +2,17 @@ package scorex.network.peer
 
 import java.net.InetSocketAddress
 
-import akka.actor.ActorRef
 import scorex.network._
-
 
 object PeerManager {
 
   case class AddPeer(address: InetSocketAddress)
-
-  case class Connected(socketAddress: InetSocketAddress, handlerRef: ActorRef,
-                       ownSocketAddress: Option[InetSocketAddress], inbound: Boolean)
-
-  case class Handshaked(address: InetSocketAddress, handshake: Handshake)
-
-  case class Disconnected(remote: InetSocketAddress)
 
   case class AddToBlacklist(address: InetSocketAddress)
 
   case class GetRandomPeersToBroadcast(howMany: Int)
 
   case class ConnectedPeers(peers: Set[ConnectedPeer])
-
-  case class PeerConnection(handlerRef: ActorRef, handshake: Option[Handshake], inbound: Boolean)
-
-  case class BlackListUpdated(host: String)
-
-  case class RegisterBlacklistListener(listener: ActorRef)
-
-  case class UnregisterBlacklistListener(listener: ActorRef)
-
-  case class ExistedBlacklist(hosts: Seq[String])
-
-  case class Suspect(address: InetSocketAddress)
-
-  case object CheckPeers
 
   case object GetAllPeers
 
@@ -44,15 +21,5 @@ object PeerManager {
   case object GetConnectedPeers
 
   case object GetConnectedPeersTyped
-
-  case object GetConnections
-
-  private case object MarkConnectedPeersVisited
-
-  case object BlacklistResendRequired
-
-  case object CloseAllConnections
-
-  case object CloseAllConnectionsComplete
 
 }
